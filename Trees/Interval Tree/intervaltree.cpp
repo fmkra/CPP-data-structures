dca9f74b070a5b50a71204513c97bc3e1c9a3b0f
kotmawszy <kotmawszy@gmail.com>
2021-02-18 21:55:49 +0100

    Change some variable naming

--- Trees/Interval Tree/intervaltree.cpp
+++ Trees/Interval Tree/intervaltree.cpp
@@ -1,34 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
+#ifndef INTERVALTREE_CPP
+#define INTERVALTREE_CPP
+// those lines prevent 'redefinition of variable' error
+
+#include <limits>
+#include <iostream>
 
 template <typename T>
-struct ITComparator {
+struct IntervalTreeOperation {
   struct Sum {
-    static const T neutralValue = 0;
+    static const T identityElement = 0;
     static T operation(const T& a, const T& b) {
       return a+b;
     }
   };
   struct Max {
-    static const T neutralValue = numeric_limits<T>::min();
+    static const T identityElement = std::numeric_limits<T>::min();
     static T operation(const T& a, const T& b) {
-      return max(a,b);
+      return std::max(a,b);
     }
   };
   struct Min {
-    static const T neutralValue = numeric_limits<T>::max();
+    static const T identityElement = std::numeric_limits<T>::max();
     static T operation(const T& a, const T& b) {
-      return min(a,b);
+      return std::min(a,b);
     }
   };
   struct Product {
-    static const T neutralValue = 1;
+    static const T identityElement = 1;
     static T operation(const T& a, const T& b) {
       return a*b;
     }
   };
   struct Xor {
-    static const T neutralValue = 0;
+    static const T identityElement = 0;
     static T operation(const T& a, const T& b) {
       return a^b;
     }
@@ -37,7 +41,7 @@ struct ITComparator {
 
 template<
   typename T,
-  class Comparator = typename ITComparator<T>::Sum
+  class Operation = typename IntervalTreeOperation<T>::Sum
 >
 class IntervalTree {
   unsigned int _size; // number of elements in tree
@@ -46,17 +50,17 @@ class IntervalTree {
   T* d;
 
   T query(unsigned int a, unsigned int b, unsigned int v, unsigned int p, unsigned int q) {
-    if(q<a || p>b) return Comparator::neutralValue;
+    if(q<a || p>b) return Operation::identityElement;
     if(p>=a && q<=b) return d[v];
     unsigned int r = (p+q)/2;
-    return Comparator::operation(
+    return Operation::operation(
       query(a,b,2*v,p,r),
       query(a,b,2*v+1,r+1,q)
     );
   }
 public:
-  // constructor from array
-  IntervalTree(const unsigned int size = 0, T fill = Comparator::neutralValue, T fillrest = Comparator::neutralValue) {
+  // constructor from size
+  IntervalTree(const unsigned int size = 0, T fill = Operation::identityElement, T fillrest = Operation::identityElement) {
     _size = size;
     _width = 1;
     _height = 1;
@@ -67,9 +71,10 @@ public:
     d = new T[2*_width];
     for(unsigned int i=0;i<_size;i++) d[_width+i]=fill;
     for(unsigned int i=_size;i<_width;i++) d[_width+i]=fillrest;
-    for(unsigned int i=_width-1;i>0;i--) d[i]=Comparator::operation(d[2*i],d[2*i+1]);
+    for(unsigned int i=_width-1;i>0;i--) d[i]=Operation::operation(d[2*i],d[2*i+1]);
   }
-  IntervalTree(const T* array, const unsigned int size, T fillrest = Comparator::neutralValue) {
+  // constructor from array
+  IntervalTree(const T* array, const unsigned int size, T fillrest = Operation::identityElement) {
     _size = size;
     _width = 1;
     _height = 1;
@@ -80,14 +85,14 @@ public:
     d = new T[2*_width];
     for(unsigned int i=0;i<_size;i++) d[_width+i]=array[i];
     for(unsigned int i=_size;i<_width;i++) d[_width+i]=fillrest;
-    for(unsigned int i=_width-1;i>0;i--) d[i]=Comparator::operation(d[2*i],d[2*i+1]);
+    for(unsigned int i=_width-1;i>0;i--) d[i]=Operation::operation(d[2*i],d[2*i+1]);
   }
   T query(unsigned int p, unsigned int q) {
     return query(p,q,1,1,_width);
   }
   void set(unsigned int p, T x) {
     d[p+=_width-1]=x;
-    while(p/=2) d[p]=Comparator::operation(d[2*p],d[2*p+1]);
+    while(p/=2) d[p]=Operation::operation(d[2*p],d[2*p+1]);
   }
   void push_back(T x) {
     if(_size++<_width) {
@@ -99,26 +104,21 @@ public:
     _width*=2;
     d = new T[2*_width];
     for(unsigned int i=0;i<_width/2;i++) d[_width+i]=tmp[i];
-    delete tmp;
+    delete[] tmp;
     d[3*_width/2]=x;
-    for(unsigned int i=_width/2+1;i<_width;i++) d[_width+i]=Comparator::neutralValue;
-    for(unsigned int i=_width-1;i>0;i--) d[i]=Comparator::operation(d[2*i],d[2*i+1]);
+    for(unsigned int i=_width/2+1;i<_width;i++) d[_width+i]=Operation::identityElement;
+    for(unsigned int i=_width-1;i>0;i--) d[i]=Operation::operation(d[2*i],d[2*i+1]);
   }
   void print() {
     unsigned int p = 2;
-    for(int i=1;i<2*_width;i++) {
-      cout<<d[i]<<" ";
+    for(unsigned int i=1;i<2*_width;i++) {
+      std::cout<<d[i]<<" ";
       if(i==p-1) {
-        cout<<"\n";
+        std::cout<<"\n";
         p*=2;
       }
     }
   }
 };
 
-int main() {
-  IntervalTree<int,ITComparator<int>::Max> a(4,0);
-  a.print();
-  a.push_back(4);
-  a.print();
-}
+#endif
