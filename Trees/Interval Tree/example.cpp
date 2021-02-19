38e37b4a166956bd9a8de183d03edf6cdf665227
kotmawszy <kotmawszy@gmail.com>
2021-02-19 18:23:51 +0100

    Update example.cpp

--- Trees/Interval Tree/example.cpp
+++ Trees/Interval Tree/example.cpp
@@ -1,8 +1,11 @@
-#include "intervaltree.cpp"
+#include "../../include/intervaltree"
 
 int main() {
-  IntervalTree<int,IntervalTreeOperation<int>::Max> a(4,0);
-  a.print();
-  a.push_back(4);
-  a.print();
+  int array[8] = {1,4,-2,7,0,2,3,2};
+  IntervalTree<int> tree(array,8); // Sum tree created from array
+
+  std::cout << tree.query(2,6) << "\n"; // answer is 11 (4-2+7+0+2)
+  std::cout << tree.query(4,7) << "\n"; // answer is 12 (7+0+2+3)
+  tree.set(5,6); // sets 5th element to 6
+  std::cout << tree.query(4,7) << "\n"; // answer is 18 (7+6+2+3)
 }
