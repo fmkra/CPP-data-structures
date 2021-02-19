79c0378ccddba33cddda7f6f6833ec967229bced
kotmawszy <kotmawszy@gmail.com>
2021-02-19 21:29:36 +0100

    Change example for List

--- Linear/List/example.cpp
+++ Linear/List/example.cpp
@@ -3,8 +3,16 @@
 
 int main() {
     List<int> l;
-    l.push_front(2);
-    l.push_front(3);
-    l.push_front(4);
-    std::cout << l.front() << " " << l.back() << "\n";
+    l.push_front(3); // list: 3
+    l.push_front(2); // list: 2 3
+    l.push_back(4);  // list: 2 3 4
+    l.push_back(5);  // list: 2 3 4 5
+    l.push_front(1); // list: 1 2 3 4 5
+    std::cout << l.front() << "\n"; // prints 1
+    std::cout << l.back() << "\n";  // prints 5
+    l.pop_front(); // list: 2 3 4 5
+    l.pop_back();  // list: 2 3 4
+    l.pop_front(); // list: 3 4
+    std::cout << l.front() << "\n"; // prints 3
+    std::cout << l.back() << "\n";  // prints 4
 }
